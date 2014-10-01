f09a32d3d6a944f1bfe5d22c8d2c93ccef7d4f35
Lukas Berglin <lukbe154@southfork-14.edu.isy.liu.se>
2014-10-01 09:11:18 +0200

    cornerBlending and internpolaeValues

---
+++ Blending/cornerBlending.m
@@ -0,0 +1,37 @@
+gridSize = 16;
+
+finalGrid = zeros(gridSize,gridSize,3);
+
+
+for i = 1:gridSize
+    for j = 1:gridSize
+        
+        diffX = i/gridSize;
+        diffY = j/gridSize;
+        
+        interpolatedX1 = interpolateValues(1,0,diffX);
+        interpolatedX2 = interpolateValues(0,0,diffX);
+        
+        finalGrid(i,j,1) = interpolateValues(interpolatedX1,interpolatedX2,diffY);
+        
+        interpolatedX1 = interpolateValues(0,1,diffX);
+        interpolatedX2 = interpolateValues(0,0,diffX);
+        
+        finalGrid(i,j,2) = interpolateValues(interpolatedX1,interpolatedX2,diffY);
+
+        interpolatedX1 = interpolateValues(0,0,diffX);
+        interpolatedX2 = interpolateValues(1,0,diffX);
+        
+        finalGrid(i,j,3) = interpolateValues(interpolatedX1,interpolatedX2,diffY);
+
+        %interpolatedX1 = interpolateValues(0,0,diffX);
+        %interpolatedX2 = interpolateValues(0,1,diffX);
+        
+        %finalGrid(i,j,4) = interpolateValues(interpolatedX1,interpolatedX2,diffY);
+        
+    end
+end
+
+
+
+imagesc(finalGrid);
