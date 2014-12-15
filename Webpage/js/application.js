0d8130d796f6d959456101a9ccf76fbcbaeb0034
Robin <robinsilverhav@gmail.com>
2014-12-15 16:42:10 +0100

    menu fix

--- Webpage/js/application.js
+++ Webpage/js/application.js
@@ -9,10 +9,13 @@ $(function(){
 
 function hashchanged(){
   var hash = location.hash.replace( /^#/, '' );
+    $(".active").toggleClass("active");
   if(hash == ""){
     $("#mainView").load("home.html");
+    $(".home").toggleClass("active");
   }else{
     $("#mainView").load(hash + ".html");
+    $("." + hash + "").toggleClass("active");
     
   }
   document.body.scrollTop = document.documentElement.scrollTop = 0;
